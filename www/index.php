e1a3924e761c33fe4c51152f702aa169cb9be847
Olivier Meunier <oli.meunier@gmail.com>
2020-10-14 15:55:51 +0200

    Formulaire Enregistrer Avatar + recup dans save.php

--- www/index.php
+++ www/index.php
@@ -2,8 +2,19 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Initialisation des valeurs des options taille et nombre de couleurs
 $size = 4;
-$colors = ['royalblue', 'crimson']; // On pourrait ici tirer des couleurs aléatoires...
+$numberOfColors = 2; // On pourrait ici tirer des couleurs aléatoires...
+
+// Si le formulaire est soumis, on va récupérer les données 
+if(!empty($_GET)){
+
+    $size = $_GET['size'];
+    $numberOfColors = $_GET['number-of-colors'];
+}
+
+// Création d'un tableau de couleurs aléatoires
+$colors = ColorHelper::getRandomColors($numberOfColors);
 
 $avatar = new AvatarMatrix($size, $colors);
 $renderer = new SvgRenderer();
---
+++ www/save.php
@@ -0,0 +1,9 @@
+<?php 
+
+// On récupère le code source SVG
+$svg = $_POST['svg'];
+
+// Génération d'un nom de fichier aléatoire et unique
+
+
+// Enregistrement du fichier dans un dossier avatars/
